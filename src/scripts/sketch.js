0f75ff788a0ce3bff0430b81bc344af99555aca5
Dominik Reisach <praises_sauces.0i@icloud.com>
2024-10-27 13:28:49 +0100

    Update background design

--- src/scripts/sketch.js
+++ src/scripts/sketch.js
@@ -4,64 +4,89 @@ See LICENSE-MIT for details
 */
 
 let canvas;
-let angle = 0;
+var points = [];
+// var mult = 0.001; // Controls the speed of the movement
+let circleDiameter;
+let thickness;
+
+var r_1;
+var g_1;
+var b_1;
+var r_2;
+var g_2;
+var b_2;
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
 function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
-  canvas.position(0, 0);
-  canvas.style("z-index", "-1");
-  canvas.style("position", "fixed");
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.position(0, 0);
+    canvas.style("z-index", "-1");
+    canvas.style("position", "fixed");
 
-  noStroke();
+    background("#000000");
+
+    angleMode(DEGREES);
+    noiseDetail(1);
+    
+    var density = 200;
+    var space = windowWidth / density;
+    
+    for (var x = 0; x < windowWidth; x += space) { // corrected condition
+        for (var y = 0; y < windowHeight; y += space) { // corrected condition
+            var p = createVector(x + random(-50, 50), y + random(-50, 50));
+            points.push(p);
+        }
+    }
+
+    shuffle(points, true);
+
+    r_1 = random(255);
+    g_1 = random(255);
+    b_1 = random(255);
+    r_2 = random(255);
+    g_2 = random(255);
+    b_2 = random(255);
+
+    mult = random(0.001, 0.005);
 }
 
-function draw() {
-  background("#000000");
+function draw() {    
+    noStroke();
 
-  let circleX = windowWidth / 2;
-  let circleY = windowHeight / 2;
-  let circleDiameter;
+    if (windowWidth < 760) {
+        circleDiameter = windowWidth / 2;
+        thickness = 1.5;
+      } else {
+        circleDiameter = windowWidth / 4;
+        thickness = 2.5;
+      }
 
-  if (windowWidth < 760) {
-    circleDiameter = windowWidth / 1.25;
-  } else {
-    circleDiameter = windowWidth / 3;
-  }
-
-
-  // Glow effect behind the circle
-  let maxGlowSize = circleDiameter * 2.5;
-  let minGlowSize = circleDiameter;
-  let glowLayers = 120; // Number of glow layers
-
-  for (let i = 0; i < glowLayers; i++) {
-    let t = i / glowLayers;
-    let glowSize = lerp(maxGlowSize, minGlowSize, t);
-
-    let col;
-
-    if (i < glowLayers / 4) {
-      col = "#f92f8b";
-    } else if (i >= glowLayers / 4 && i < glowLayers / 4 * 2) {
-      col = "#fcbe37";
-    } else if (i >= glowLayers / 4 * 2 && i < glowLayers / 4 * 3) {
-      col = "#992d9b";
+    if (frameCount * 20 <= points.length) {
+        var max = frameCount * 20;
     } else {
-      col = "#187caa";
+        var max = points.length;
     }
+
+    for (var i = 0; i < max; i++) {
+    // for (var i = 0; i < points.length; i++) {
+        // var r = map(points[i].x, 0, windowWidth, 50, 255);
+        // var g = map(points[i].y, 0, windowHeight, 50, 255);
+        // var b = map(points[i].x, 0, windowWidth, 255, 50);
+        var r = map(points[i].x, 0, windowWidth, r_1, r_2);
+        var g = map(points[i].y, 0, windowHeight, g_1, g_2);
+        var b = map(points[i].x, 0, windowWidth, b_1, b_2);
+        var alpha = map(dist(windowWidth / 2, windowHeight / 2, points[i].x, points[i].y), 0, circleDiameter, 255, 0);
     
-    let c = color(col);
-    c.setAlpha(6); // Set transparency for the glow
+        fill(r, g, b, alpha);
 
-    fill(c);
-    ellipse(circleX, circleY, glowSize, glowSize);
-  }
+        var angle = map(noise(points[i].x * mult, points[i].y * mult), 0, 1, 0, 720);
+        points[i].add(createVector(cos(angle), sin(angle)));
 
-  // Draw the black circle on top of the glow
-  fill("#000000");
-  circle(circleX, circleY, circleDiameter);
+        if (dist(windowWidth / 2, windowHeight / 2, points[i].x, points[i].y) < circleDiameter) {
+            ellipse(points[i].x, points[i].y, thickness);
+        }
+    }
 }
